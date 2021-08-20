c40a439d3aeb40202ac5cc7539333e07a9a4ff9a
Norawit Nangsue <norawitn@hotmail.com>
2021-08-20 16:26:13 +0700

    add setbool

--- scripts/sos_service.py
+++ scripts/sos_service.py
@@ -4,6 +4,9 @@ import rospy                                      # the main module for ROS-pyth
 from std_srvs.srv import Trigger, TriggerResponse # we are creating a 'Trigger service'...
                                                   # ...Other types are available, and you can create
                                                   # custom types
+from std_srvs.srv import SetBool, SetBoolResponse # we are creating a 'Trigger service'...
+                                                  # ...Other types are available, and you can create
+                                                  # custom types
 def trigger_response(request):
     ''' 
     Callback function used by the service server to process
@@ -14,11 +17,32 @@ def trigger_response(request):
         message="Hey, roger that; we'll be right there!"
     )
 
+def setbool_response(request):
+    ''' 
+    Callback function used by the service server to process
+    requests from clients. It returns a TriggerResponse
+    '''
+    m = ""
+    if request.data:
+        m = "police is coming"
+    else:
+        m = "police won't come"
+
+    return SetBoolResponse(
+        success=request.data,
+        message=m
+    )
+
 rospy.init_node('sos_service')                     # initialize a ROS node
 
-rospy.loginfo('SOS Service Run')
+rospy.loginfo('SOS Service Run 2.0')
 
 my_service = rospy.Service(                        # create a service, specifying its name,
     '/fake_911', Trigger, trigger_response         # type, and callback
 )
+
+my_service2 = rospy.Service(                        # create a service, specifying its name,
+    '/true_191', SetBool, setbool_response         # type, and callback
+)
+
 rospy.spin()                                       # Keep the program from exiting, until Ctrl + C is pressed
